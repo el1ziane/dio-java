59cdd0a47e567504439ee120655db51a99d6b1b7
Eliziane <eliziane@edu.unifil.br>
2023-10-01 18:07:55 -0300

    Tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1; // variavel
+        numero = 2;
+        System.out.println(numero);
+
+        final double VALOR_PI = 3.14; //constante (variavel que não pode ser alterada)
+        
+
+
+    }   
 }
